e17c881fc7221bc9c1cbd573800b3a9d63f1ff51
Estella Ye <72925272+xcye515@users.noreply.github.com>
2022-04-13 21:52:57 -0400

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -36,17 +36,6 @@ DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/w4111"
 #
 engine = create_engine(DATABASEURI)
 
-
-# Here we create a test table and insert some values in it
- engine.execute("""DROP TABLE IF EXISTS test;""")
- engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-
-
-
 @app.before_request
 def before_request():
   """
