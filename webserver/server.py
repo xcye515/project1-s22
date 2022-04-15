7f09a889df90b11a9d53675e5067961272994cf5
Xingchen Ye <xcye2001@dyn-129-236-178-120.dyn.columbia.edu>
2022-04-14 22:14:08 -0400

    test

--- webserver/server.py
+++ webserver/server.py
@@ -76,16 +76,17 @@ def search():
   player_name = request.args["player_name"]
   
   if player_name != '':
+    player_name_tmp = "%" + player_name + "%"
     if request.args["attr"] == "ability":
-      query = text("SELECT p.username, p.ability FROM Player AS p WHERE p.username LIKE '%s'" % player_name)
+      query = text("SELECT p.username, p.ability FROM Player AS p WHERE p.username LIKE '%s'" % player_name_tmp)
     elif request.args["attr"] == "uid":
-      query = text("SELECT p.username, p.uid FROM Player AS p WHERE p.username LIKE '%s'" % player_name)
+      query = text("SELECT p.username, p.uid FROM Player AS p WHERE p.username LIKE '%s'" % player_name_tmp)
     elif request.args["attr"] == "wid":
-      query = text("SELECT p.username, pinw.world_id FROM Player AS p, Player_in_World as pinw WHERE p.uid = pinw.uid AND p.username LIKE '%s'" % player_name)
+      query = text("SELECT p.username, pinw.world_id FROM Player AS p, Player_in_World as pinw WHERE p.uid = pinw.uid AND p.username LIKE '%s'" % player_name_tmp)
     elif request.args["attr"] == "exp":
-      query = text("SELECT p.username, p.exp FROM Player AS p WHERE p.username LIKE '%s'" % player_name)
+      query = text("SELECT p.username, p.exp FROM Player AS p WHERE p.username LIKE '%s'" % player_name_tmp)
     else:
-      query = text("SELECT p.username, p.uid, pinw.world_id, p.exp, p.ability FROM Player AS p, Player_in_World AS pinw WHERE p.uid = pinw.uid AND p.username LIKE '%s'" % player_name)
+      query = text("SELECT p.username, p.uid, pinw.world_id, p.exp, p.ability FROM Player AS p, Player_in_World AS pinw WHERE p.uid = pinw.uid AND p.username LIKE '%s'" % player_name_tmp)
   else:
     if request.args["attr"] == "ability":
       query = "SELECT p.username, p.ability FROM Player AS p"
